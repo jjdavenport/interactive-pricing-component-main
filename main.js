47a93003a577478b3ad733e47dbd871d75e1aa2d
jjdavenport <jordanjamesdavenport@gmail.com>
2024-05-15 19:56:14 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const slider = document.getElementById("slider");
+const toggleSwitch = document.getElementById("toogle-switch");
+
+const views = [];
+const priceYearly = [];
+const priceMonthly = [];

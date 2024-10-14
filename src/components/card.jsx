e2420369b46b6ab5800a221aa39ed29d62069992
jjdavenport <jordanjamesdavenport@gmail.com>
2024-10-14 14:37:19 +0100

    add pricing function

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,27 +1,47 @@
 import { useState } from "react";
 import checkIcon from "./assets/icon-check.svg";
 
-const Card = ({}) => {
-  const [monthly, setMonthly] = useState(true);
-  const [yearly, setYearly] = useState(false);
-  const toggle = () => setYearly(!yearly);
+const Card = () => {
+  const [billing, setBilling] = useState("month");
+  const [input, setInput] = useState(50);
+  const toggle = () => setBilling(billing === "month" ? "year" : "month");
   const views = ["10K", "50K", "100K", "500K", "1M"];
   const priceMonthly = [8, 12, 16, 24, 36];
-  const priceyearly = [6, 9, 12, 18, 27];
+  const priceYearly = [6, 9, 12, 18, 27];
+
+  const change = (e) => {
+    setInput(parseInt(e.target.value));
+  };
+
+  const index = Math.floor(input / 25);
+  const pageViews = views[index];
+  const price = billing === "year" ? priceYearly[index] : priceMonthly[index];
+
   return (
     <>
       <main className="flex flex-col gap-2">
-        <h3>Pageviews</h3>
-        <input className="cursor-pointer" type="range" min="0" max="100" />
-        <h2></h2>
+        <h3>
+          {pageViews} <span className="uppercase">Pageviews</span>
+        </h3>
+        <input
+          value={input}
+          onChange={change}
+          className="cursor-pointer"
+          type="range"
+          min="0"
+          max="100"
+        />
+        <h2>
+          ${price}.00 / {billing}
+        </h2>
         <div className="flex gap-4">
           <label>
             Monthly Billing
-            <button></button>
+            <button className="h-4 w-4" onClick={toggle}></button>
           </label>
           <label>
-            Yearly Billing
-            <button></button>
+            Yearly Billing -25%
+            <button className="h-4 w-4" onClick={toggle}></button>
           </label>
         </div>
         <ul>
